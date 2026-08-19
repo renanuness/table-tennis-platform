385cd13a52f435352d2aeacdf0df5efaa248fe91
renanuness <renan1504@gmail.com>
2026-08-19 13:41:57 -0300

    gateway setup

---
+++ infrastructure/config-server/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
